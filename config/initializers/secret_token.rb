df7ef57c277436b1e72fbffb12fab4cfa4b46d44
Keith Young Johnson <keithjohnson@Keiths-MacBook-Pro.local>
2013-04-18 16:51:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carouser::Application.config.secret_token = '631598311e7ffbb9848a005f0bcb6b75f756c5b3e2fbd84040dfed8645d7276bc1dfccf1182e7580b80e93ac6a5ae8488aaf0172ea073ad23dc8fcf1b154740f'
